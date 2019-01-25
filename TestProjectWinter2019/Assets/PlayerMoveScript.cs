c2e56f04f74df56ef4338bba3dabb4f5ba112a69
Charles Cue <cuecharles@gmail.com>
2019-01-24 16:56:26 -0800

    Upload test print message.

--- TestProjectWinter2019/Assets/PlayerMoveScript.cs
+++ TestProjectWinter2019/Assets/PlayerMoveScript.cs
@@ -6,7 +6,8 @@ public class PlayerMoveScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        print("My name is Charles, I have no idea what to write. ");
+
 	}
 	
 	// Update is called once per frame
